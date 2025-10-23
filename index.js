a1a28736c9081632c80b318118ade1fea9e8f902
BrownBearDancing <143032898+BrownBearDancing@users.noreply.github.com>
2025-10-23 14:38:59 -0500

    all but last 2

---
+++ index.js
@@ -0,0 +1,9 @@
+let sum= 1 + 4;
+let difference = sum - 2;
+let product = difference * 5;
+let quotient= product / 2;
+let remainder= 
+
+
+should capture the remainder of product remainder of 2 in a variable named remainder
+should assign the innerHTML of the HTML element with the id result to the quotient
