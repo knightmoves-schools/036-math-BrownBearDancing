8fd676e015508e8963d6224254935c09ac1505bf
BrownBearDancing <143032898+BrownBearDancing@users.noreply.github.com>
2025-10-23 14:45:36 -0500

    check this

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ let quotient= product / 2;
 document.getElementById("result").innerHTML= quotient % 2;
 
 
-should capture the remainder of product remainder of 2 in a variable named remainder
-should assign the innerHTML of the HTML element with the id result to the quotient
+
+
 
