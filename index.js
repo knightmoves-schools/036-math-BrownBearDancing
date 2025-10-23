feec75514771e71f04128367c333e3e848cb4490
BrownBearDancing <143032898+BrownBearDancing@users.noreply.github.com>
2025-10-23 14:43:12 -0500

    check this

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ let sum= 1 + 4;
 let difference = sum - 2;
 let product = difference * 5;
 let quotient= product / 2;
-let remainder= 
+
+
+document.getElementById("result").innerHTML= quotient % 2;
 
 
 should capture the remainder of product remainder of 2 in a variable named remainder
 should assign the innerHTML of the HTML element with the id result to the quotient
+
